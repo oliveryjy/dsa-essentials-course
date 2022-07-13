15e975227b408e9a9d0c1737cb073643da1358c9
oliveryjy <yjyoliver@hotmail.com>
2022-07-13 11:22:33 +0800

    git push

--- 02 Arrays/01array.cpp
+++ 02 Arrays/01array.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-	int marks[100] = {-1};
+	int marks[100] = {-1}; // Create array of 100 elements, initialise first element as -1.
 
 	int n;
 	cout<<"Enter the no of students ";
@@ -13,18 +13,19 @@ int main(){
 
 
 	//Input
-	for(int i=1; i<=n; i++){
+	for(int i=1; i<=n; i++){ // Start from index 1, end at n.
 		cin>> marks[i];
 
+		//Update
 		marks[i] = marks[i]*2;
 	}
 
-	//Update 
-
-
+	
 	//Output
-	for(int i=0;i<100;i++){
-		cout<< marks[i] <<" ,";
+	for(int i=0;i<100;i++){ // From index 1 to 99, print the marks
+		cout<< marks[i] <<", "; 
+		// Note that the unassigned indexes will be garbage values (for declared only arrays).
+		// UNLESS one or more elements have been initalised, then the rest of the elements will be zero. (like line 8)
 	}
 
 	cout <<endl;
--- 02 Arrays/02array.cpp
+++ 02 Arrays/02array.cpp
@@ -2,11 +2,23 @@
 #include<iostream>
 using namespace std;
 
-void printArray(int * arr,int n){
+// Note that inside the main function, we have the actual array.
+// But if we pass in an array into a function, we only have the address of the array. This is known as pass by reference (which means the array is not passed by value).
 
+// Using this as an example: void printArray(int arr[]){...}:
+// If we ""cout << sizeof(arr)"" with the function taking in ""int arr[]"", it will return 8 bytes. Why? 
+// Because in this case, variable arr is a pointer that holds the array address. And a pointer size is 8 bytes.
+
+// If you want to make it more explicit, you cna use "int *arr" instead of "int arr[]".
+
+void printArray(int *arr,int n){ 
+	// "int *arr" to be more explicit that an address is being passed
+	// "int n" to get the correct number of elements as calculated in main.
 
 	cout <<"In Function "<<sizeof(arr) <<endl;
-	arr[0] = 100;
+
+	arr[0] = 100; // Update the 0th index of the array to 100
+
 	for(int i=0;i<n;i++){
 		cout << arr[i] <<endl; 
 	}
@@ -19,19 +31,18 @@ int main(){
 	int arr[] = {1,2,3,4,5,6};
 
 	int n = sizeof(arr)/sizeof(int);
+	// int = 4 bytes
+	// array has 6 int elements = 24 bytes
+	// sizeof(arr)/sizeof(int) = 24 bytes / 4 bytes = 6 elements
 
-
-	
 	printArray(arr,n);
-
 	
 	cout <<"In Main "<<sizeof(arr) <<endl;
-	for(int i=0;i<n;i++){
+
+	for(int i=0;i<n;i++){ // Print out array elements
 		cout << arr[i] <<endl; 
 	}
 
-	
-
-
 	return 0;
 }
+
