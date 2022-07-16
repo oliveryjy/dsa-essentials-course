19fabc3d56288e82694f9ba4587ed72b57c67474
oliveryjy <yjyoliver@hotmail.com>
2022-07-16 19:01:15 +0800

    updated

--- 03 Basic Sorting /bubble_sort.cpp
+++ 03 Basic Sorting /bubble_sort.cpp
@@ -11,8 +11,8 @@ void bubble_sort(int a[],int n){
 
 	for(int times=1;times<=n-1;times++){
 		//repeated swapping
-		for(int j=0; j <= n - times - 1; j++ ){
-			if(compare(a[j],a[j+1])){
+		for(int j=0; j <= n - times - 1; j++ ){ // Compare up to index n - 1 - times
+			if(compare(a[j],a[j+1])){ // If a[j] > a[j+1], then swap.
 				swap(a[j],a[j+1]);
 			}
 		}
@@ -27,8 +27,11 @@ int main(){
 	int arr[] = {-2,3,4,-1,5,-12,6,1,3};
 	int n = sizeof(arr)/sizeof(int);
 	
-
 	bubble_sort(arr,n);
+
+	for (auto x : arr){
+		cout << x << ",";
+	}
 
 	
 
